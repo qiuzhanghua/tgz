242bd53663788d13a0f22970a9dade0900b8cbe3
qiuzhanghua <qiuzhanghua@icloud.com>
2023-11-09 11:52:42 +0800

    add LOGGING_FORMAT support

--- init.go
+++ init.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"github.com/qiuzhanghua/common/log"
+	"github.com/labstack/gommon/log"
+	"github.com/qiuzhanghua/common/util"
 	"os"
+	"strings"
 )
 
 func init() {
+	log.SetPrefix("tgz")
 	level := os.Getenv("LOGGING_LEVEL")
-	log.SetGlobalLevel(level)
+	if level == "" {
+		level = "off"
+	}
+	format := strings.ToLower(os.Getenv("LOGGING_FORMAT"))
+	if format != "json" {
+		log.SetHeader(`${time_rfc3339_nano}, ${prefix}, ${level} ${short_file}(${line})`)
+	}
+	log.SetLevel(util.LevelOf(level))
+	log.SetOutput(os.Stdout)
 }
